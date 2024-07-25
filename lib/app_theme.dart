8c06a61cd2fb84457336427af60776367273ae9f
Omar Assem <omarassem.route@gmail.com>
2024-07-25 20:47:17 +0300

    Implement nav bar & quran tab

---
+++ lib/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color lightPrimary = Color(0xFFB7935F);
+  static const Color darkPrimary = Color(0xFF141A2E);
+  static const Color white = Color(0xFFF8F8F8);
+  static const Color black = Color(0xFF242424);
+  static const Color gold = Color(0xFFFACC1D);
+
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: lightPrimary,
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: lightPrimary,
+      selectedItemColor: black,
+      unselectedItemColor: white,
+    ),
+    textTheme: TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+        color: black,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: black,
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData();
+}
